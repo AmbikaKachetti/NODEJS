66aedb19f4c96bc462bceee1f5e2d9ebb0e5dfdb
Ambika Rama Katyayini <35074062+AmbikaKachetti@users.noreply.github.com>
2024-12-13 21:38:36 +0530

    REST API

--- day5/server.js
+++ day5/server.js
@@ -1,15 +1,36 @@
 const express = require('express');
-
 const app = express();
-
 app.listen(4500, ()=>{
     console.log('Server started');
 })
 // GET METHOD
-app.get('/hello', (req, res)=>{
+app.get('/higet', (req, res)=>{
     res.send("Hello recived GET request")
 })
+app.get('/', (req, res)=>{
+    // here we need to give absolute path
+    res.send(`
+        <ol>
+            <li>list 1</li>
+            <li>list 2</li>
+            <li>list 3</li>
+        </ol>
+        <h1>Thi is heading</h1>
+    `) 
+})
+app.get('/', (req, res)=>{
+    // here we need to give absolute path
+    res.sendFile(__dirname + '/index.html')
+})
 // POST METHOD
-app.post('/hello', (req, res)=>{
+app.post('/hipost', (req, res)=>{
     res.send("Hello received POST request");
+}) 
+// PUT METHOD
+app.put('/hiput', (req, res)=>{
+    res.send("Hello received PUT request");
+})
+// DELETE METHOD
+app.delete('/hidelete', (req, res)=>{
+    res.send("Hello received DELETE request");
 })
