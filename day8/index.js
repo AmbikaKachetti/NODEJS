def41e62525b209ea5042e588089a458f155cfc6
Ambika Rama Katyayini <35074062+AmbikaKachetti@users.noreply.github.com>
2024-12-22 20:54:16 +0530

    Nodemailer sending emails

--- day8/index.js
+++ day8/index.js
@@ -1,1 +1,127 @@
 var nm = require('nodemailer');
+var transporter = nm.createTransport(
+    {
+        host: "smtp.gmail.com",
+        port: 587,
+        secure: false,
+        auth:{
+            user: "demo47942@gmail.com",
+            pass: "pass-key",
+        }
+    }
+);
+var options = {
+    from: "demo47942@gmail.com",
+    to: "kkatyaini1@gmail.com",
+    subject: "test mail from nodemailer module",
+    // text: "Nodeemail sending 'nodemailer' test message"
+    html:`<h1>Welcome to NodeJS Application</h1>
+    <img src='cid:food' width: '200px'>
+    <button>Click Here</button>
+    `,
+    attachments:[
+        {
+            filename: 'food.jpg',
+            path: 'https://images.pexels.com/photos/842571/pexels-photo-842571.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+            cid: 'food'
+        }
+    ]
+}
+transporter.sendMail(
+    options, function(error_info, success_info){
+        if(error){
+            console.log(error_info);
+        }
+        else{
+            console.log("Send Successfully");
+        }
+    }
+)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- day8/readme.md
+++ day8/readme.md
@@ -1,2 +1,7 @@
 # Send Email with Nodemailer library
 - nodemialder has "createTransport" 
+
+- here in sender email: we have to gener app password after turinging on the 2 step authentication.
+    - here in y case not working with test demo email.
+    - uptill undertood perfectly
+    = pixel for image used as copy iage addess like any other image source
