1fecb78ea55ca92f1e8b507fba34382a5c58c117
Ambika Rama Katyayini <35074062+AmbikaKachetti@users.noreply.github.com>
2024-12-27 18:16:56 +0530

    CORS error in NODEJS

--- day10/day10/index.js
+++ day10/day10/index.js
@@ -1,8 +1,47 @@
 const express = require("express")
 const app = express()
+var cors = require('cors')
+//! Method 1
+// app.use(cors({
+//     'methods': 'GET, POST',
+//     'origin': "*",
+//     'allowedHeaders': 'content-type'
+// }));
+//! Method 2
+// this means all the options are available to all the servers
+app.use(cors()) // or app.use(cors('*'))
+let users = [
+    {
+        "id": 1,
+        "name": "Leanne Graham1",
+        "email": "Sincere1@april.biz",
+        "phone": "1-770-736-8031 x56441"
+    },
+    {
+        "id": 2,
+        "name": "Leanne Graham2",
+        "email": "Sincere2@april.biz",
+        "phone": "1-770-736-8031 x56442"
+    },
+    {
+        "id": 3,
+        "name": "Leanne Graham3",
+        "email": "Sincere3@april.biz",
+        "phone": "1-770-736-8031 x56443"
+    },
+    {
+        "id": 4,
+        "name": "Leanne Graham4",
+        "email": "Sincere4@april.biz",
+        "phone": "1-770-736-8031 x56444"
+    }
+]
 app.get('/users', (req, res)=>{
-    res.send("Get Success")
-})
-app.listen(4000, ()=>{
+    // res.send("Get Success")
+    // !Method 1
+    // res.setHeader('Access-Control-Allow-Origin', 'http://127.0.0.1:5500')
+    res.send(users);
+});
+app.listen(2000, ()=>{
     console.log("Started");
 })
--- day10/day10/readme.md
+++ day10/day10/readme.md
@@ -4,8 +4,8 @@
     - like withdrawing money using sbi card in baroda bank atm.
 -   <script>
         // fetch('http://localhost:4000/users') 
-        ip address + port number = domain name, example amazon.com, flipkart.com
-        127.0.0.1  + 4000
+        - ip address + port number = domain name, example amazon.com, flipkart.com
+        - 127.0.0.1  + 4000
             - here index.html is running at 5500 port 
             - and index.js is running at 4000 port
         - running in defferent server - here we get CORS error
@@ -17,4 +17,14 @@
         .then(res => res.json())
         .then(data => console.log(data))
     </script>
-- this is the meaning for index.html, that we placed in
+- this is the meaning for index.html, that we placed outsid of the folder
+# Allowing CORS issue 
+1. Method 1 default way
+app.get('/users', (req, res)=>{
+    // res.send("Get Success")
+    res.setHeader('Access-Control-Allow-Origin', 
+        'http://127.0.0.1:5500')
+    res.send(users);
+});
+2. Method 2 Express.JS libraries, we can call it as middlewares
+- CORS is on library or middleware.
