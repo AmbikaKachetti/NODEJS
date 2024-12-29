a8d34c72950c8face9b6af53391ee281d033b616
Ambika Rama Katyayini <35074062+AmbikaKachetti@users.noreply.github.com>
2024-12-29 23:58:02 +0530

    Multer

--- day11/index.js
+++ day11/index.js
@@ -1,21 +1,29 @@
 const express = require('express');
 const multer = require('multer');
 
+const cors = require('cors');
+
 const app = express();
-const storage = multer.diskStorage({
-    destination: 'uploads/',
+
+app.use(cors());
+
+// this is diskStorage type Object in multer
+const ds = multer.diskStorage({
+    destination: 'day11/uploads',
     filename: (req, file, cb) => {
         cb(null, Date.now() + '-' + file.originalname)
     }
 })
-multer({
-    storage:
-    dest: 'upload/'
+// this is actual multer object 
+const upload = multer({
+    storage: ds,
+    dest: '/upload/'
 })
 app.get('/', (req, res)=>{
     res.send('Hi')
 })
-app.post('/upload', (req, res)=>{
+app.post('/upload', upload.single('avatar'),
+    (req, res)=>{
     res.send()
 })
 
